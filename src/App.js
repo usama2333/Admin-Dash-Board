8ab96d25adcb3fcbf88ce0691a3a15089add45ab
usama2333 <usama.wizz@gmail.com>
2023-06-08 14:10:38 +0500

    dashboard

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -36,10 +36,8 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-             <Route path="/line" element={<Line />} />
-            
-             
-              {/* <Route path="/geography" element={<Geography />} />  */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} /> 
             </Routes>
           </div>
         </div>
